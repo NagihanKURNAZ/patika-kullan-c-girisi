7fedbe7087a25f0101a12f3e06b0a132975766e0
NagihanKURNAZ <112775522+NagihanKURNAZ@users.noreply.github.com>
2022-12-31 11:18:45 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,63 +1,50 @@
-import java.nio.channels.SelectableChannel;
 import java.util.Scanner;
 
-import org.w3c.dom.ls.LSOutput;
+import javax.naming.ldap.ExtendedResponse;
+import javax.print.DocFlavor.INPUT_STREAM;
 
 public class Main {
 
 	public static void main(String[] args) {
-		
-		String userName = "patika";
-		String password = "Java123";
-		String select;
-		String newPassword;
-		boolean check = true;
-		
+
+		int matematik, fizik, türkçe, kimya, müzik;
+		int toplam = 0;
+		int ders_sayısı = 0;
+
 		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Kullanıcı  Adınızı Giriniz: ");
-		userName=input.nextLine();
-		System.out.println("Şifrenizi Giriniz: ");
-		password=input.nextLine();
-		
-		if(userName.equals("patika")&&password.equals("Java123")){
-			System.out.println("giriş yaptınız.");
+
+		System.out.println("Matematik notunuzu giriniz: ");
+		matematik = input.nextInt();
+		if (matematik > 0 && matematik <= 100) {
+			toplam = toplam + matematik;
+			ders_sayısı++;
 		}
-			else if(userName!="patika") {
-				System.out.println("Kullanıcı adınızı yanlış girdiniz.");	
-			}
-			else if(password!="Java123") {
-				System.out.println("şifrenizi yanlış girdiniz.");
-				
-			}
-		while (check){
-            System.out.print("Şifrenizi değiştirmek ister misiniz? (y/n)"); int a = input.nextLine().charAt(0);
-            switch (a){
-                case 'y' :
-                    System.out.print("Lütfen yeni şifrenizi giriniz:"); newPassword = input.nextLine();
-                    if (newPassword.equals("Java123") || newPassword.equals(password)){
-                        System.out.println("Şifreniz eski şifreniz ile aynı olamaz. Üst menüye aktarılıyorsunuz.");
-                    }
-                    else {
-                        System.out.println("Yeni şifreniz başarılı bir şekilde oluşturuldu.");
-                        System.out.println("Çıkış yapılıyor...");
-                        check = false;
-                        break;
-                    }
-                    break;
-                case 'n':
-                    System.out.println("Çıkış yapılıyor..");
-                    check = false;
-                    break;
-                default:
-                    System.out.println("Hatalı işlem seçtiniz. Üst menüye aktarılıyorsunuz.");
-		
+
+		System.out.println("Türkçe notunuzu giriniz: ");
+		türkçe = input.nextInt();
+		if (türkçe > 0 && türkçe <= 100) {
+			toplam = toplam + türkçe;
+			ders_sayısı++;
 		}
-		
-	}
-		
-		
-		
+
+		System.out.println("Fizik notunuzu giriniz: ");
+		fizik = input.nextInt();
+
+		System.out.println("Kimya notunuzu giriniz: ");
+		kimya = input.nextInt();
+
+		System.out.println("Müzik notunuzu giriniz: ");
+		müzik = input.nextInt();
+
+		double average = toplam / ders_sayısı;
+
+		if (average <= 55) {
+			System.out.println("Sınıfta kaldınız");
+		} else {
+			System.out.println("Sınıfı başarıyla geçtiniz");
+		}
+
+		System.out.println("Ortalamanız: " + average);
 	}
 
 }
